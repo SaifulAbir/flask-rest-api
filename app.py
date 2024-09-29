39c28eaf7a9f85b02391ce272bfe3749f6187a8c
Saiful Islam <saiful.abir20@gmail.com>
2024-09-29 02:42:59 +0200

    video resource created

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 names = {"tim": {"age": 19, "gender": "male"}, "bill":{"age": 23, "gender": "male"}}
+videos = {}
 
 class HelloWorld(Resource):
     def get(self, name):
         return names[name]
 
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form)
+        return {}
+
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -4,3 +4,6 @@ BASE = "http://127.0.0.1:5000/"
 
 response = requests.get(BASE + "helloworld/tim")
 print(response.json())
+
+video_response = requests.put(BASE + "video/1", {"likes": 10})
+print(video_response.json())
